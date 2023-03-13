3f86b7aa5673b91ddfb0bfa8c4e955531688dd2e
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-14 02:06:57 +0800

    updated

--- random/index.js
+++ random/index.js
@@ -5,26 +5,27 @@ const { BASE10_CHARSET, BASE16_CHARSET, BASE64WEB_CHARSET } = require('../baseCo
 
 const selfMT = MT();
 
-const randInt = (start, end) => {
+const rand = (mt) => (mt || selfMT).random();
+
+const randInt = (start, end, mt) => {
   if (!end) end = start, start = 0;
-  return Math.floor(start + selfMT.random() * end);
+  return Math.floor(start + rand(mt) * end);
 };
 
 const random = {
-  randInt,
-  rand: () => selfMT.random(),
-  charset: (charset, len=8) => new Array(len).fill(0).map(l => random.choice(charset)).join(''),
-  base10: (len=6) => random.charset(BASE10_CHARSET, len),
-  base16: (len) => random.charset(BASE16_CHARSET, len),
-  base64: (len) => random.charset(BASE64WEB_CHARSET, len),
-  choice: (array) => array[randInt(array.length)],
-  shuffle: (array) => random.choices(array, array.length),
-  choices(array, amount=1) {
+  rand, randInt,
+  charset: (charset, len=8, mt) => new Array(len).fill(0).map(l => random.choice(charset, mt)).join(''),
+  base10: (len=6, mt) => random.charset(BASE10_CHARSET, len, mt),
+  base16: (len, mt) => random.charset(BASE16_CHARSET, len, mt),
+  base64: (len, mt) => random.charset(BASE64WEB_CHARSET, len, mt),
+  choice: (array, mt) => array[randInt(0, array.length, mt)],
+  shuffle: (array, mt) => random.choices(array, array.length, mt),
+  choices(array, amount=1, mt) {
     const result = [];
     const options = [];
     for (let i = 0; i < amount; i++) {
       if (!options.length) options.push(...array);
-      result.push(options.splice(randInt(options.length), 1)[0]);
+      result.push(options.splice(randInt(0, options.length, mt), 1)[0]);
     };
     return result;
   },
