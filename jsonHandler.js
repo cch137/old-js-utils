202169a9a858b40ab6022b15470077fd8f1e8fd3
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-15 03:20:31 +0800

    updated

--- jsonHandler.js
+++ jsonHandler.js
@@ -8,15 +8,19 @@ class JSONHandler extends DataHandler {
   #data = {};
   #lastModified = 0;
   get data() {
-    const mtimeMs = statSync(this.filepath).mtimeMs;
-    if (this.#lastModified < mtimeMs) {
-      const data = JSON.parse(readFileSync(this.filepath, 'utf8'));
-      this.#lastModified = mtimeMs;
-      for (const k of this.keys()) this.remove(k);
-      for (const k in data) this.set(k, data[k]);
-      return this.#data = data;
-    } else {
-      return this.#data;
+    try {
+      const mtimeMs = statSync(this.filepath).mtimeMs;
+      if (this.#lastModified < mtimeMs) {
+        const data = JSON.parse(readFileSync(this.filepath, 'utf8'));
+        this.#lastModified = mtimeMs;
+        for (const k of this.keys()) this.remove(k);
+        for (const k in data) this.set(k, data[k]);
+        return this.#data = data;
+      } else {
+        return this.#data;
+      }
+    } catch {
+      return {};
     }
   }
   set data(value) {return this.#data = value, this.onchange(value), value}
