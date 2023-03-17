50177cc044fe3c7529333d33ff4446aed4cdaffe
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-17 13:10:15 +0800

    updated

--- index.js
+++ index.js
@@ -23,22 +23,13 @@ const floor = (num, digits=0) => {
 }
 
 const chee = {
+  // # ARRAY
   assign: (tar, src) => Object.assign(tar, src),
   unique: (arr) => [...new Set(arr)],
   isIterable,
   isArray,
   toArray: (value) => isIterable(value) ? [...value] : [value],
   packArray: (value) => (isIterable(value) && typeof value !== 'string') ? [...value] : [value],
-  /** @param {Number|Number[]} numbers */
-  sum: (...numbers) => {
-    numbers = numbers.flat();
-    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  },
-  /** @param {Number|Number[]} numbers */
-  avg: (...numbers) => {
-    numbers = numbers.flat();
-    return chee.sum(numbers) / numbers.length;
-  },
   /** @param {Number} a @param {Number} [b] @param {Number} [c] */
   range(a, b, c) {
     const numbers = [];
@@ -55,11 +46,47 @@ const chee = {
     }
     return numbers;
   },
+  /** @param {Array} arr @param {Number} [size] */
+  chunk(arr, size=1) {
+    const result = [];
+    arr = [...arr];
+    while (arr.length) result.push(arr.splice(0, size));
+    return result;
+  },
+  /** @param {Array} arr @param {Number} [quantity] */
+  allocate(arr, quantity=1) {
+    const result = Array.from({ length: quantity }).map(e => []);
+    arr = [...arr].reverse();
+    while (arr.length) {
+      for (const r of result) {
+        if (!arr.length) break;
+        r.push(arr.pop());
+      }
+    }
+    return result;
+  },
+
+  // # STRING
   str: (v) => `${v}`,
   lower: (s) => `${s}`.toLowerCase(),
   upper: (s) => `${s}`.toUpperCase(),
   capitalize: (s) => `${chee.upper(s[0])}${s.substring(1)}`,
+  escapeString: (str) => JSON.stringify(str).slice(1, -1),
+
+  // # MATH
   ceil, round, floor,
+  /** @param {Number|Number[]} numbers */
+  sum: (...numbers) => {
+    numbers = numbers.flat();
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  },
+  /** @param {Number|Number[]} numbers */
+  avg: (...numbers) => {
+    numbers = numbers.flat();
+    return chee.sum(numbers) / numbers.length;
+  },
+
+  // # OBJECT
   trimObj(obj) {
     if (isArray(obj)) {
       for (let i = 0; i < obj.length; i++) if (typeof obj === 'object') obj[i] = trimObj(obj[i]);
@@ -82,8 +109,10 @@ const chee = {
     };
     return JSON.stringify(obj, reviver);
   },
+
   SECRET_KEY: '#31|02u()27I>',
-  escapeString: (str) => JSON.stringify(str).slice(1, -1),
+
+  // # CONVERTS
   formatBytes(fileSizeByte=0, toFix=2, spaceBfrUnit=true) {
     const d = parseInt(Math.log(fileSizeByte) / Math.log(1024)) || 0;
     return `${(fileSizeByte/Math.pow(1024, d>5?5:d)).toFixed(toFix)}${spaceBfrUnit?' ':''}${['','K','M','G','T','P'][d>5?5:d]}B`;
