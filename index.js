3d7df262ee93dbdc94ce89d6798a56318c02a7c1
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-17 13:55:47 +0800

    updated

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const chee = {
   // # CONVERTS
   formatBytes(fileSizeByte=0, toFix=2, spaceBfrUnit=true) {
     const d = parseInt(Math.log(fileSizeByte) / Math.log(1024)) || 0;
-    return `${(fileSizeByte/Math.pow(1024, d>5?5:d)).toFixed(toFix)}${spaceBfrUnit?' ':''}${['','K','M','G','T','P'][d>5?5:d]}B`;
+    return `${(fileSizeByte/Math.pow(1024, d>5?5:d)).toFixed(toFix)}${spaceBfrUnit?' ':''}${['',...'KMGTP'][d>5?5:d]}B`;
   },
 };
 
