413778712bab1938dbe9b73d27908b96f1ce54ea
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-25 22:28:19 +0800

    updated

--- random/index.js
+++ random/index.js
@@ -14,55 +14,61 @@ const randInt = (start, end, mt) => {
   return Math.floor(start + rand(mt) * end);
 };
 
+const choice = (array, mt) => array[randInt(0, array.length, mt)];
+const choices = (array, amount=1, mt) => {
+  const result = [];
+  const options = [];
+  for (let i = 0; i < amount; i++) {
+    if (!options.length) options.push(...array);
+    result.push(options.splice(randInt(0, options.length, mt), 1)[0]);
+  };
+  return result;
+}
+
+const shuffle = (array, mt) => choices(array, array.length, mt);
+
 const maskingCharsetGenerator = (charset, mt) => {
   return () => {
-    charset = random.shuffle(charset, mt);
+    charset = shuffle(charset, mt);
     return charset.join('');
   }
 }
 
+/** @param {String} string @returns {String} */
+const mask = (string, charset=16, level=1) => {
+  const realCharset = getCharset(charset);
+  const seed1 = randInt(0, charset);
+  const mt1 = MT(seed1);
+  const generator = maskingCharsetGenerator(realCharset, MT(randInt(0, 1000000, mt1)));
+  const characters = typeof string === 'string' ? string.split('') : string;
+  const result = [
+    convert(seed1, 10, charset),
+    ...characters.map(char => generator()[realCharset.indexOf(char)])
+  ];
+  if (--level < 1) return result.join('');
+  return mask(result, charset, level);
+}
+
+/** @param {String} string @returns {String} */
+const unmask = (string, charset=16, level=1) => {
+  const realCharset = getCharset(charset);
+  const seed = +convert(string[0], charset, 10);
+  const mt1 = MT(seed);
+  const generator = maskingCharsetGenerator(realCharset, MT(randInt(0, 1000000, mt1)));
+  const characters = (typeof string === 'string' ? string.split('') : string).slice(1, string.length);
+  const result = characters.map(char => realCharset[generator().indexOf(char)]);
+  if (--level < 1) return result.join('');
+  return unmask(result, charset, level);
+}
+
+const charset = (charset, len=8, mt) => new Array(len).fill(0).map(l => choice(charset, mt)).join('');
+
 const random = {
-  rand, randInt,
-  charset: (charset, len=8, mt) => new Array(len).fill(0).map(l => random.choice(charset, mt)).join(''),
-  base10: (len=6, mt) => random.charset(BASE10_CHARSET, len, mt),
-  base16: (len, mt) => random.charset(BASE16_CHARSET, len, mt),
-  base64: (len, mt) => random.charset(BASE64WEB_CHARSET, len, mt),
-  choice: (array, mt) => array[randInt(0, array.length, mt)],
-  shuffle: (array, mt) => random.choices(array, array.length, mt),
-  /** @param {String} string @returns {String} */
-  mask(string, charset=16, level=1) {
-    const realCharset = getCharset(charset);
-    const seed1 = randInt(0, charset);
-    const mt1 = MT(seed1);
-    const generator = maskingCharsetGenerator(realCharset, MT(randInt(0, 1000000, mt1)));
-    const characters = typeof string === 'string' ? string.split('') : string;
-    const result = [
-      convert(seed1, 10, charset),
-      ...characters.map(char => generator()[realCharset.indexOf(char)])
-    ];
-    if (--level < 1) return result.join('');
-    return random.mask(result, charset, level);
-  },
-  /** @param {String} string @returns {String} */
-  unmask(string, charset=16, level=1) {
-    const realCharset = getCharset(charset);
-    const seed = +convert(string[0], charset, 10);
-    const mt1 = MT(seed);
-    const generator = maskingCharsetGenerator(realCharset, MT(randInt(0, 1000000, mt1)));
-    const characters = (typeof string === 'string' ? string.split('') : string).slice(1, string.length);
-    const result = characters.map(char => realCharset[generator().indexOf(char)]);
-    if (--level < 1) return result.join('');
-    return random.unmask(result, charset, level);
-  },
-  choices(array, amount=1, mt) {
-    const result = [];
-    const options = [];
-    for (let i = 0; i < amount; i++) {
-      if (!options.length) options.push(...array);
-      result.push(options.splice(randInt(0, options.length, mt), 1)[0]);
-    };
-    return result;
-  },
+  rand, randInt, charset,
+  base10: (len=6, mt) => charset(BASE10_CHARSET, len, mt),
+  base16: (len, mt) => charset(BASE16_CHARSET, len, mt),
+  base64: (len, mt) => charset(BASE64WEB_CHARSET, len, mt),
+  choice, choices, shuffle, mask, unmask,
   /** Linear Congruential Generator */
   lcg(seed) {
     seed = toSeed(seed);
